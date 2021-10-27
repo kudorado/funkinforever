0d7bf4b47281698e18a826e6ff3eafbc0e144cc0
Tai <tmtudev@gmail.com>
2021-10-27 17:23:42 +0700

    update 3

--- source/fmf/songs/SongManager.hx
+++ source/fmf/songs/SongManager.hx
@@ -21,17 +21,6 @@ class SongManager
 			}
 		),
 
-//-------------------------DADDY DEAREST---------------------
-		new SongData
-		(
-			{
-				folder: 'week_nonsense',
-				character: 'nonsense',
-				songTitle: 'V.S Nonsense',
-				songList: ['Common-sense', 'Highlights', 'Context', "Nonsensical"]
-			}
-		),
-	
 		new SongData
 		(
 			{
@@ -66,45 +55,12 @@ class SongManager
 		new SongData
 		(
 			{
-<<<<<<< HEAD
-=======
-				folder: 'week_hex',
-				character: 'hex',
-				songTitle: 'V.S Hex',
-				songList: ['Dunk', 'Ram', 'Hello-world', 'Glitcher', 'Encore']
-			}
-		),
-
-		new SongData
-		(
-			{
 				folder: 'week_mfm',
 				character: 'mfm',
 				songTitle: 'Mid-Fight Masses',
 				songList: ['Parish', 'Worship', "Zavodila", 'Gospel', 'Casanova']
 			}
 		),
-
-		new SongData
-		(
-			{
-				folder: 'week_impostor',
-				character: 'red',
-				songTitle: 'V.S Red Impostor',
-				songList: ['Sussus-moogus', 'Sabotage', 'Meltdown']
-			}
-		),
-
-		new SongData
-		(
-			{
-				folder: 'week_impostor',
-				character: 'green',
-				songTitle: 'V.S Green Impostor',
-				songList: ['Sussus-toogus', 'Lights-down', 'Reactor', 'Ejected']
-			}
-		),
-
 	
 		new SongData
 		(
@@ -120,7 +76,6 @@ class SongManager
 		new SongData
 		(
 			{
->>>>>>> d4cfe3e2f33832632bb8c544a60e1c8c2cecf2a4
 				folder: 'week_entity',
 				character: 'daidem',
 				songTitle: 'Entity Origins',
