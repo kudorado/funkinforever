5e993b36d506794d7e6ee4547f0b68ee58be1ce2
datvt2236 <datk3b@gmail.com>
2021-10-30 23:10:35 +0700

    ejected

--- source/fmf/songs/Ejected.hx
+++ source/fmf/songs/Ejected.hx
@@ -1,5 +1,6 @@
 package fmf.songs;
 
+import lime.math.BGRA;
 import js.html.rtc.PeerConnectionIceEvent;
 import flixel.util.FlxColor;
 import flixel.FlxG;
@@ -11,93 +12,73 @@ class Ejected extends SongPlayer
 {
     override function getDadTex()
 	{
-		var tex = Paths.getSparrowAtlas('pc/sus/crewmate', 'mods');
+		var tex = Paths.getSparrowAtlas('pc/sus/parasite', 'mods');
 		dad.frames = tex;
+	}
+
+    override  function getGFTex() {
+		var text = Paths.getSparrowAtlas('gf_skins/parachute/gf_parachute', 'mods');
+		gf.frames = text;
 	}
 
 	override function loadMap()
 	{
 
-		playState.defaultCamZoom = 0.8;
+		playState.defaultCamZoom = 0.7;
 
-		var bg1:FlxSprite = new FlxSprite(0, -300).loadGraphic(Paths.image('bg/impostor/red/polusSky', 'mods'));
+		var bg1:FlxSprite = new FlxSprite(-1000, -1200).loadGraphic(Paths.image('bg/sus/ejected1/sky', 'mods'));
 		bg1.antialiasing = true;
-		bg1.scale.y = 1.4;
-		bg1.scale.x = 1.4;
+		bg1.scale.y = 1;
+		bg1.scale.x = 1;
 		playState.add(bg1);
 
-        
-
-        var bg3:FlxSprite = new FlxSprite(-100, -100).loadGraphic(Paths.image('bg/impostor/red/polusrocks', 'mods'));
-		bg3.antialiasing = true;
-		bg3.scale.y = 1;
-		bg3.scale.x = 1;
-        bg3.scrollFactor.set(0.6,0.6);
-		playState.add(bg3);
-
-
-        var bg2:FlxSprite = new FlxSprite(200, 150).loadGraphic(Paths.image('bg/impostor/red/polusWarehouse', 'mods'));
-		bg2.antialiasing = true;
-		bg2.scale.y = 1;
-		bg2.scale.x = 1;
-        bg2.scrollFactor.set(0.8,0.8);
-		playState.add(bg2);
-        
-
-        var bg3:FlxSprite = new FlxSprite(-250, 300).loadGraphic(Paths.image('bg/impostor/red/polusHills', 'mods'));
-		bg3.antialiasing = true;
-		bg3.scale.y = 1;
-		bg3.scale.x = 1;
-        bg3.scrollFactor.set(0.8,0.8);
-		playState.add(bg3);
-
-        var bg4:FlxSprite = new FlxSprite(-0, 600).loadGraphic(Paths.image('bg/impostor/red/polusGround', 'mods'));
-		bg4.antialiasing = true;
-		bg4.scale.y = 1.4;
-		bg4.scale.x = 1.4;
-		playState.add(bg4);
+        var fgClouds:FlxSprite = new FlxSprite(-1150, 500).loadGraphic(Paths.image('bg/sus/ejected1/fgClouds', 'mods'));
+		fgClouds.antialiasing = true;
+		fgClouds.scale.y = 1;
+		fgClouds.scale.x = 1;
+		playState.add(fgClouds);
 
 	}
 
 	override function createDadAnimations():Void
 	{
 		var animation = dad.animation;
-		animation.addByPrefix('idle', 'impostor idle00', 18, false);
-		animation.addByPrefix('singUP', 'impostor up20', 24, false);
-		animation.addByPrefix('singDOWN', 'impostor down00', 24, false);
-		animation.addByPrefix('singLEFT', 'imposter left00', 24, false);
-		animation.addByPrefix('singRIGHT', 'impostor right00', 24, false);
+		animation.addByPrefix('idle', 'Mungus idle00', 18, false);
+		animation.addByPrefix('singUP', 'Parasite up new00', 24, false);
+		animation.addByPrefix('singDOWN', 'Paracite down00', 24, false);
+		animation.addByPrefix('singLEFT', 'Parasite left00', 24, false);
+		animation.addByPrefix('singRIGHT', 'Parasite right new00', 24, false);
 		dad.animation = animation;
 
 	}
 
 	override function createDadAnimationOffsets():Void
 	{
-		dad.addOffset('idle', -83, -27);
-		dad.addOffset("singUP", 49, 67);
-		dad.addOffset("singRIGHT", -61, -40);
-		dad.addOffset("singLEFT", 116, -21);
-		dad.addOffset("singDOWN", 21, -53);
+		dad.addOffset('idle', -0, -0);
+		dad.addOffset("singUP", 0, 0);
+		dad.addOffset("singRIGHT", -0, -0);
+		dad.addOffset("singLEFT", 0, -0);
+		dad.addOffset("singDOWN", 0, -0);
 		dad.dance();
 
-		dad.scale.x = 0.9;
-		dad.scale.y = 0.9;
+		dad.scale.x = 2;
+		dad.scale.y = 2;
 		dad.x -= 500;
-		dad.y += 225;
+		dad.y += 425;
 	}
 
 	override function createGFAnimationOffsets()
 	{
 		super.createGFAnimationOffsets();
-		
+		gf.scale.x = 1;
+        gf.scale.y = 1;
 	}
 
 	override function createBFAnimationOffsets()
 	{
 		super.createBFAnimationOffsets();
-		
 		bf.x += 150;
-        bf.y += 50;
+        bf.y += 350;
 	}
 
 	public override function getDadIcon(icon:HealthIcon)
