77a59086d633feadba930d972bdf4202f60c2040
datvt2236 <datk3b@gmail.com>
2021-12-17 21:37:59 +0700

    huggy

---
+++ source/fmf/songs/PlayTime.hx
@@ -0,0 +1,96 @@
+package fmf.songs;
+
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import MenuCharacter.CharacterSetting;
+import fmf.characters.*;
+
+class PlayTime extends SongPlayer
+{
+    override function getDadTex()
+	{
+		var tex = Paths.getSparrowAtlas('pc/huggy_wuggy/huggy', 'mods');
+		dad.frames = tex;
+	}
+
+	override function loadMap()
+	{
+        playState.defaultCamZoom = 0.6;
+		var bg:FlxSprite = new FlxSprite(-669, -362).loadGraphic(Paths.image('bg/huggy_wuggy/factory', 'mods'));
+		bg.antialiasing = true;
+		bg.scale.y = 1.2;
+		bg.scale.x = 1.2;
+
+        var bg1:FlxSprite = new FlxSprite(-669, -362).loadGraphic(Paths.image('bg/huggy_wuggy/shadow', 'mods'));
+		bg1.antialiasing = true;
+		bg1.scale.y = 1.2;
+		bg1.scale.x = 1.2;
+
+		playState.add(bg);
+	}
+
+	override function createDadAnimations():Void
+	{
+		var animation = dad.animation;
+		animation.addByPrefix('idle', 'huggyidle00', 18, false);
+		animation.addByPrefix('singUP', 'huggyup00', 24, false);
+		animation.addByPrefix('singRIGHT', 'huggyright00', 24, false);
+		animation.addByPrefix('singLEFT', 'huggyleft00', 24, false);
+		animation.addByPrefix('singDOWN', 'huggydown00', 24, false);
+		dad.animation = animation;
+
+	}
+
+	override function createDadAnimationOffsets():Void
+	{
+
+        dad.addOffset('idle',30, 220);
+
+		dad.addOffset('singUP', 224, 221);
+		dad.addOffset('singRIGHT', 80, 196);
+		dad.addOffset('singLEFT', 80, 157);
+		dad.addOffset('singDOWN', 110, -100);
+		dad.dance();
+        dad.scale.x = 1;
+        dad.scale.y = 1;
+		dad.x = 117;
+		dad.y = 296;
+	
+	}
+
+    
+
+	override function createBFAnimationOffsets()
+	{
+		super.createBFAnimationOffsets();
+		bf.y = 458;
+		bf.x = 948;
+	}
+
+	override function createGFAnimationOffsets()
+	{
+		super.createGFAnimationOffsets();
+	}
+
+	override function updateCamFollowBF()
+	{
+		playState.camFollow.x = gf.getGraphicMidpoint().x + 120;
+		playState.camFollow.y = 360;
+
+	}
+
+	override function updateCamFollowDad()
+	{
+		playState.camFollow.x = gf.getGraphicMidpoint().x - 120;
+		playState.camFollow.y = 360;
+
+	}
+
+    public override function getDadIcon(icon:HealthIcon)
+    {
+        icon.loadGraphic(Paths.image('health_icon/icon_huggy/icon-huggy', 'mods'), true, 150, 150);
+        icon.animation.add('dad', [0, 1], 0, false, false);
+        icon.animation.play("dad");
+    }
+}
